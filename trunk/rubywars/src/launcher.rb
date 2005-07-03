e5ab152fd7b5f6ecb7ccfdcabf4087648ebdf0bb
jcroisant <jcroisant@630b0ccc-50f8-0310-abe2-8f1ad513eeb2>
2005-07-03 05:54:22 +0000

    - Fix reference errors in Launcher#shoot (@parent.groups[], @lifespan) - Add attr_accessors for @parent, @type, and @lifespan. - Add documentary comments for all methods.

--- trunk/rubywars/src/launcher.rb
+++ trunk/rubywars/src/launcher.rb
@@ -1,28 +1,36 @@
-# Launcher file
+# Launcher class definitions
 #
 # This is one part of:
 #
 # Rubywars (working title) --  A scrolling space shooter game
 # Copyright (C) Greg Colombo, John Croisant 2005
-#
-# "NFL Sunday Ticket / This is how watching football games should be / It's NFL Sunday Ticket / on DirecTV"
 
 require 'src/vector'
 
 class Launcher
+	attr_accessor :parent, :type, :lifespan
 
-	def initialize(parent, type, spawnedLifespan)
+	# Create a new Launcher object, "attached" to a parent object.
+	# 
+	# +parent+::    object to "attach" the Launcher to.
+	# +type+::      class of Projectile the Launcher creates.
+	# +lifespan+::  how long (milliseconds) the launched Projectiles persist.
+	def initialize(parent, type, lifespan)
 		@parent = parent
 		@type = type
-		@lifespan = spawnedLifespan
+		@lifespan = lifespan
 	end
 
-        def shoot
-                t = (Rubygame::Time.get_ticks() - @parent.stamp[:p])/1000.0
-                v = Vector.new(1,0).rotate(@parent.angle)
-                v.magnitude += 150
-                p = @parent.project(t)
-                @groups[0].push(@type.new(p,v,lifespan))
-        end
-
+	# Spawn and launch a new Projectile object. The Projectile will inherit
+	# the Launcher's parent's position and velocity (plus some extra velocity).
+	# 
+	# The class and lifespan of the Projectile are governed by @type and
+	# @lifespan, which are set when the Launcher is created.
+	def shoot
+		t = (Rubygame::Time.get_ticks() - @parent.stamp[:p])/1000.0
+		v = Vector.new(1,0).rotate(@parent.angle)
+		v.magnitude += 150
+		p = @parent.project(t)
+		@parent.groups[0].push(@type.new(p,v,@lifespan))
+	end
 end
